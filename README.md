4fc971a70bdc18bc652881b7dcd22a1b47a181c9
Alexandre Miyazaki <alexandre.miyazaki@corp.globo.com>
2020-06-02 09:17:38 -0300

    first exercises

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spark_bd_essentials
+# Spark Big Data Essentials
+Code in this repo is associated with the Udemy Course https://www.udemy.com/course/spark-essentials/. Basically, it is 
+the solution to the exercises.
---
+++ src/main/scala/exercises1/ExercisesDFEssentials.scala
@@ -0,0 +1,30 @@
+package exercises1
+
+import org.apache.spark.sql.SparkSession
+
+object ExercisesDFEssentials extends App {
+  val spark = SparkSession.builder()
+    .appName("Exercise 1")
+    .master("local[*]")
+    .getOrCreate()
+
+  val smf = Seq(
+    ("Xiaomi", "Mi A3", "6\"", 10),
+    ("Samsung", "S10", "6\"", 15),
+    ("Motorola", "Moto G22", "5.5\"", 10)
+  )
+
+  import spark.implicits._
+  val df1 = smf.toDF("Make", "Model", "Screen Size", "Camera MP")
+  df1.show()
+  df1.printSchema()
+
+  val df2 = spark.read
+    .format("json")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/movies.json")
+
+  df2.show()
+  df2.printSchema()
+  println(s"Rows in df: ${df2.count()}")
+}
